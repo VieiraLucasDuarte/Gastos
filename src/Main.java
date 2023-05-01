58e1c32424e8fe05bc38a5c9f614ff8b33637abb
Lucas <lucasduartevieira0@gmail.com>
2023-05-01 20:03:02 -0300

    Primeiro Commit, criado menu e algumas rotinas

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
